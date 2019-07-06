b72b1e12b6dcef007a28626ab1989fb825ade205
zhangchong <zhangchong0862@163.com>
2019-07-06 22:38:39 +0800

    修改host

--- VIPCourse/xfzes/xfz/xfz/settings.py
+++ VIPCourse/xfzes/xfz/xfz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+4&^4b7uq@f!aay^@o=w0&pg$-ys$)q$l6dk51#u6yk+4*lvve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','184.170.219.63']
 
 
 
