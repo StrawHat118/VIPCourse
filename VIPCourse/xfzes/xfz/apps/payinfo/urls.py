8f637d3f01475801a5488346eafb753f38401f6e
zhangchong <zhangchong0862@163.com>
2019-07-10 23:41:38 +0800

    注释pay_order

--- VIPCourse/xfzes/xfz/apps/payinfo/urls.py
+++ VIPCourse/xfzes/xfz/apps/payinfo/urls.py
@@ -7,7 +7,6 @@ app_name = 'payinfo'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('payinfo_order/',views.payinfo_order,name='payinfo_order'),
-    path('notify_url/',views.notify_view,name='notify_view'),
-    path('download/',views.download,name='download')
+    # path('notify_url/',views.notify_view,name='notify_view'),
+    # path('download/',views.download,name='download')
 ]
